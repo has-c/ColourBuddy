91b0dc82da3079d7b4007ae86454f673743cda83
Hasnain Cheena <you@example.com>
2017-07-04 16:57:17 +1200

    Added basis camera ability

---
+++ Module2/Module2/Module2.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Module2.Droid.MainApplication, Module2.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5b2a689d58fef72d9ff47668a1e9510f7.MainApplication.class, md5b2a689d58fef72d9ff47668a1e9510f7.MainApplication.__md_methods);
+		
+	}
+}
